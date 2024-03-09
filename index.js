77112fd316484409a44d9b0abb55333d93828d57
Jose Miguel Paniagua Tinajero <ttn.developer@gmail.com>
2024-03-08 18:32:23 -0600

    cleaning

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const express = require('express');
+const express = require('express')
 
-var app = express()
+const app = express()
 
 const port = 8000
 
 app.get('/', function(_, res) {
-    res.send('<h1>Página de inicio</h1>');
-});
+    res.send('<h1>Página de inicio</h1>')
+})
 
 app.get('/login', function(_, res) {
     res.send('Página de login');
-});
+})
 
 app.get('**', (_, res) => { res.send('No se encuentra esta ruta.') })
 
 app.listen(port, () => {
-    console.log(`El servidor está corriendo en: http://127.0.0.1:${port}`);
+    console.log(`El servidor está corriendo en: http://127.0.0.1:${port}`)
 })
